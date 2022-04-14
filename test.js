d6ccf88c39f4fa7799a07981a60917dcb978a02b
Constantin <constantin_goeldel@t-online.de>
2022-04-14 15:56:36 +0200

    add testing

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
-import { Validator } from 'jsonschema'
-import schema from './schema.json' assert { type: 'json'}
-import schema2 from './schema2.json' assert { type: 'json'}
-import example from './example.json' assert {type: 'json'}
-const v = new Validator()
-const result = v.validate(example, schema2)
-console.log(result.valid)
+import schema from "./schema.json" assert { type: "json" };
+import example from "./example.json" assert { type: "json" };
+import Ajv from "ajv";
+const ajv = new Ajv({ allErrors: true });
+const validate = ajv.compile(schema);
+const valid = validate(example);
+
+if (!valid) console.log(validate.errors);
+console.log(valid);
