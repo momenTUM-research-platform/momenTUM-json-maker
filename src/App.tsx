99d7fc464a495909a98ece1c215a4cf704aa6415
Constantin Goeldel <constantin_goeldel@t-online.de>
2022-05-11 19:27:09 +0200

    Adds Table of Contents (#4)
    * adds table of content to head of generator
    
    * switches from npm to yarn
    
    * adds material-ui 5 as the ui-library
    
    instead of chakra-ui for better visual aid
    
    * adds changes for material-ui5
    
    * implements authentication on upload
    
    * updates gitignore
    
    * update README
    
    * remove unncessesary code
    
    * adds github action for build ci
    
    * change directory for github actions
    
    * adds GA trigger
    
    * fix spelling error
    
    * fixes indentation
    
    * fix build error by switching to parcel as vite
    
    - reason: vite does not handle common js
    
    * CI only on PR
    
    * adds ts-config paths
    
    Co-authored-by: Constantin Goeldel <constantin.goeldel@tum.de>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import FormComponent from "@rjsf/chakra-ui";
+import { MuiForm5 as FormComponent } from "@rjsf/material-ui";
+import Ajv, { DefinedError } from "ajv";
+import { useState } from "react";
 import styled from "styled-components";
-import { CheckCircleIcon, WarningIcon } from "@chakra-ui/icons";
-import Ajv, { DefinedError, ValidateFunction } from "ajv";
-import "./App.css";
-import { useEffect, useState } from "react";
 import schema from "../schema.json";
 import { Form } from "../types";
+
+import ToC from "./ToC";
+import "./App.css";
 
 const BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://tuspl22-momentum.srv.mwn.de"
-    : "http://localhost:3000";
+    : "http://localhost:3001";
 
 const Container = styled.div`
   margin: 100px;
@@ -20,41 +21,32 @@ const Button = styled.button`
   background: #3070b3;
   color: white;
   border-radius: 5px;
-  margin-right: 5px;
-`;
-
-const P = styled.p`
-  margin-bottom: 10px;
-  font-size: 1rem;
-`;
-
-const Toolbar = styled.div`
-  display: flex;
-  postion: sticky;
+  margin-right: 7px;
+  margin-top: 3px;
+  font-size: 1em;
+  box-shadow: none;
+  border: none;
+  text-decoration: none;
 `;
 
 function App() {
   const [form, setForm] = useState<Form | null>(null);
-  const [valid, setValid] = useState(false);
-  const [invalidReason, setInvalidReason] = useState<DefinedError | null>(null);
-  const [validator, setValidator] = useState<ValidateFunction | null>();
 
   const uiSchema = {
-    title: { "ui:widget": "hidden" },
+    title: { "ui:widget": "date" },
   };
-  useEffect(() => {
-    // if( form?.properties.study_name && !form?.properties.study_id ) { auto-generate study_id
 
-    const validate = validator ? validator : new Ajv().compile(schema);
-    validator || setValidator(validate);
-    const valid = validate(form);
-
-    setValid(valid);
-    setInvalidReason(null);
+  // Validation is computationally expensive, but only done on submit/uplaod
+  function isValidForm(form: Form): { valid: boolean; msg: string } {
+    const validator = new Ajv().compile(schema);
+    const valid = validator(form);
     if (!valid) {
-      setInvalidReason(validate.errors![0] as DefinedError);
+      const errors = validator.errors as DefinedError[];
+      return { valid: false, msg: errors[0].message || "Unknown error" };
+    } else {
+      return { valid: true, msg: "Valid" };
     }
-  }, [form]);
+  }
 
   function save() {
     const data = JSON.stringify(form, null, 2);
@@ -81,21 +73,33 @@ function App() {
     input.click();
   }
 
-  async function upload() {
-    const data = JSON.stringify(form, null, 2);
-    const postURL = BASE_URL + "/surveys";
-    const response = await fetch(postURL, {
-      method: "POST",
-      body: data,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const json = await response.json();
-    console.log(json);
-    if (json.status === "ok") {
-      // @ts-ignore
-      alert("Uploaded survey with id " + json.uuid + " to " + json.uri);
+  async function upload(form: Form) {
+    const { valid, msg } = isValidForm(form);
+    if (valid) {
+      const data = JSON.stringify(form, null, 2);
+      const password = prompt(
+        "Please enter the password to upload surveys. If you don't know it, ask constantin.goeldel@tum.de or read the .env file on the server"
+      );
+      const postURL = BASE_URL + "/surveys";
+      const response = await fetch(postURL, {
+        method: "POST",
+        body: data,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: password || "MomenTUM",
+        },
+      });
+      const json = await response.json();
+      console.log(json);
+      if (json.status === "ok") {
+        // @ts-ignore
+        alert("Uploaded survey with id " + json.uuid + " to " + json.uri);
+      } else {
+        // @ts-ignore
+        alert("Error: " + json.message);
+      }
+    } else {
+      alert("Form is not valid. Error: " + msg);
     }
   }
 
@@ -116,30 +120,24 @@ function App() {
 
   return (
     <Container>
-      <P>
-        The survey is currently{" "}
-        {valid ? (
-          <>
-            {" "}
-            <CheckCircleIcon /> valid{" "}
-          </>
-        ) : (
-          <>
-            <WarningIcon /> Invalid
-          </>
-        )}
-      </P>
+      <h1>Welcome to the MomenTUM Survey Generator!</h1>
 
       <Button onClick={save}>Save JSON file</Button>
       <Button onClick={load}>Load JSON file</Button>
-      <Button onClick={upload}>Upload JSON file</Button>
+      <Button onClick={() => form && upload(form)}>Upload JSON file</Button>
       <Button onClick={download}>Download JSON file</Button>
       <Button>
-        <a href="https://github.com/TUMChronobiology/momenTUM-json-maker">Github</a>{" "}
+        <a className="github" href="https://github.com/TUMChronobiology/momenTUM-json-maker">
+          Github
+        </a>{" "}
       </Button>
       <br />
+      {form && <ToC form={form} />}
+      {/*
+ // @ts-ignore */}
       <FormComponent
         onChange={({ formData }: { formData: Form }) => setForm(formData)}
+        onSubmit={(e) => form && upload(form)}
         //@ts-ignore
         schema={schema}
         formData={form}
---
+++ src/ToC.tsx
@@ -0,0 +1,60 @@
+import { Fragment } from "react";
+import styled from "styled-components";
+import { Form } from "../types";
+
+const Inset = styled.div`
+  margin-left: 10px;
+`;
+
+export default function ToC({ form }: { form: Form }) {
+  const scroll = (id: string) => {
+    const el = document.getElementById(id);
+    el && el.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+  return (
+    <>
+      <h2>Table of contents</h2>
+      <Inset>
+        <h3>Properties</h3>
+        <h3>Modules</h3>
+        <Inset>
+          {form.modules.map((module, m_index) => (
+            <Fragment key={`form_modules_${m_index}_name-label`}>
+              <a onClick={() => scroll(`form_modules_${m_index}_name-label`)}>
+                <p>{module.name}</p>
+              </a>
+              <Inset>
+                {module.sections.map((section, s_index) => (
+                  <Fragment key={`form_modules_${m_index}_sections_${s_index}_name-label`}>
+                    <a
+                      onClick={() =>
+                        scroll(`form_modules_${m_index}_sections_${s_index}_name-label`)
+                      }
+                    >
+                      {" "}
+                      <p>{section.name}</p>
+                    </a>
+                    <Inset>
+                      {section.questions.map((question, q_index) => {
+                        <a
+                          key={`form_modules_${m_index}_sections_${s_index}_questions_${q_index}_id-label`}
+                          onClick={() =>
+                            scroll(
+                              `form_modules_${m_index}_sections_${s_index}_questions_${q_index}_id-label`
+                            )
+                          }
+                        >
+                          <p>{question.text}</p>
+                        </a>;
+                      })}
+                    </Inset>
+                  </Fragment>
+                ))}
+              </Inset>
+            </Fragment>
+          ))}
+        </Inset>
+      </Inset>
+    </>
+  );
+}
