08e7509cb076ddc1d3009ec9e9349f8c51689769
Constantin Goeldel <constantin_goeldel@t-online.de>
2022-10-17 23:32:32 -0400

    adds tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.ts"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        "main": "#3070b3"
+      },
+    },
   },
   plugins: [],
 }
