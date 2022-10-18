fbe7ba73fa086ef04a29cd0e043b22afce1c61d4
Constantin Goeldel <constantin_goeldel@t-online.de>
2022-10-17 23:34:09 -0400

    add layout component

---
+++ frontend/src/Layout.tsx
@@ -0,0 +1,49 @@
+import React, { ReactChildren, ReactElement } from "react";
+import {
+  addApiKey,
+  download,
+  generateDictionary,
+  load,
+  save,
+  upload,
+  validate,
+} from "./utils/actions";
+
+export function Layout({ children }: { children: ReactElement }) {
+  return (
+    <>
+      <header className="flex justify-between p-8 pb-6 shadow-md">
+        <div className="flex">
+          <img
+            src="/assets/icon.png"
+            alt="MomenTUM logo: A brain with a question mark"
+            className="w-10 h-10 "
+          />
+          <h1 className="text-xl p-2">MomenTUM-JSON-Maker</h1>
+        </div>
+        <div className="flex justif">
+          <Action action={save}>Save JSON file</Action>
+          <Action action={load}>Load JSON file</Action>
+          <Action action={upload}>Upload JSON file</Action>
+          <Action action={download}>Download JSON file</Action>
+          {/* <Action action={generateDictionary}>Generate RedCap Dictionary</Action>
+          <Action action={addApiKey}>Add API key</Action>
+          <Action action={validate}>Validate</Action> */}
+        </div>
+      </header>
+      {children}
+      <footer></footer>
+    </>
+  );
+}
+
+function Action({ children, action }) {
+  return (
+    <button
+      className="pointer-events-auto ml-4 rounded-md bg-main py-2 px-3 text-lg font-semibold leading-5 text-white hover:bg-sky-500"
+      onClick={action}
+    >
+      {children}
+    </button>
+  );
+}
