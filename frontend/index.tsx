cbad8c36cfc293aecb3b5205d08e5aef73e42929
Constantin Goeldel <cgoeldel@pop-os.localdomain>
2022-11-12 12:46:18 +0100

    enable dagre & immer map/set

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -2,5 +2,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./src/App";
 import React from "react";
+import { enableMapSet } from "immer";
+import Ajv from "ajv";
+import { study } from "./schema/study";
+import dagre from "dagre";
 
+
+export const validator = new Ajv().compile(study);
+export const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+enableMapSet() // Needed for immer to be able to use Maps
 ReactDOM.render(<App />, document.getElementById("root"));
