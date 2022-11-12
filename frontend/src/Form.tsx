4049aee8369dd0f45e701c0379736a51bb9a5623
Constantin Goeldel <constantin_goeldel@t-online.de>
2022-11-12 12:49:57 +0100

    simplyfy hidden fields logic

--- frontend/src/Form.tsx
+++ frontend/src/Form.tsx
@@ -1,4 +1,5 @@
 import { MuiForm5 as FormComponent } from "@rjsf/material-ui";
+import { JSONSchemaType } from "ajv";
 import React, { useMemo } from "react";
 import { module } from "../schema/module";
 import { properties } from "../schema/properties";
@@ -7,17 +8,19 @@ import { section } from "../schema/section";
 import { State, useStore } from "./state";
 
 export function Form({ id }: { id: string }) {
-  const store = useStore();
-  const [data, update, schema] = extractFormInformation(store, id);
-  console.log(data, store.study.properties);
+  const [data, update, schema] = useForm(id);
 
-  const uiSchema = {
+
+  let uiSchema = {
+    title: { "ui:widget": "date" },
     "ui:submitButtonOptions": {
       norender: true,
     },
-    title: { "ui:widget": "date" },
-    id: { "ui:widget": "hidden" },
   };
+
+  const hiddenFields = ["id", "study_id", "post_url"]
+  hiddenFields.forEach(field => uiSchema[field] = { "ui:widget": "hidden" })
+
   return (
     <FormComponent
       onChange={({ formData }) => update(formData)}
@@ -30,11 +33,11 @@ export function Form({ id }: { id: string }) {
     />
   );
 }
-function extractFormInformation(store: State, id: string): [any, any, any] {
-  console.log("Running memoization");
-  const data = store.modules[id] || store.sections[id] || store.questions[id] || store.study;
-  if (store.modules[id]) return [data, store.setModule, module];
-  if (store.sections[id]) return [data, store.setSection, section];
-  if (store.questions[id]) return [data, store.setQuestion, question];
+function useForm(id: string): [Properties | Module | Section | Question, (from: any) => void, Object] { // Returns [ current form data, changehandler to update data, schema for form ]
+  const store = useStore.getState()
+  const data = store.modules.get(id) || store.sections.get(id) || store.questions.get(id) || store.study.properties;
+  if (store.modules.get(id)) return [data, store.setModule, module];
+  if (store.sections.get(id)) return [data, store.setSection, section];
+  if (store.questions.get(id)) return [data, store.setQuestion, question];
   return [data, store.setProperties, properties];
 }
