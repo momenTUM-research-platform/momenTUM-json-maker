f06edf67c7cdb299521eb52dd7d7d3df23ab6e41
Constantin Goeldel <constantin_goeldel@t-online.de>
2022-11-14 15:18:57 +0100

    before removing grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,46 @@
 import toast, { Toaster } from "react-hot-toast";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import QR from "qrcode";
 import { Form } from "./Form";
 import { Graph } from "./Graph";
 import { Layout } from "./Layout";
 import { useStore } from "./state";
+import Drag from "../assets/drag";
 
 export const API_URL =
   process.env.NODE_ENV === "production" ? "/api/v1" : "http://localhost:8000/api/v1";
 
 function App() {
+  const [distribution, setDistribution] = useState(0.5);
 
+  const leftPx = Math.floor((window.innerWidth - 20) * distribution);
+  const rightPx = Math.floor((window.innerWidth - 20) * (1 - distribution));
+  console.log(distribution, leftPx, rightPx);
   useEffect(() => {
-    useStore.getState().calcGraphFromAtoms()
-    useStore.getState().alignNodes()
-  }, [])
+    useStore.getState().calcGraphFromAtoms();
+    useStore.getState().alignNodes();
+  }, []);
 
   const { selectedNode } = useStore();
   return (
     <Layout>
-      <main className="grid grid-cols-2 grid-col">
+      <main className={`grid`} style={{ gridTemplateColumns: `${leftPx}px 20px ${rightPx}px` }}>
         <section className="h-[calc(100vh-110px)] w-full">
           <Graph />
         </section>
-        <section className="my-8 px-2 overflow-scroll h-[calc(100vh-140px)]">
+        <section
+          draggable={true}
+          onClick={() => console.log("Clicked")}
+          onDrag={(e) => {
+            console.log("Dragging", e.clientX);
+            setDistribution(distribution + e.clientX / window.innerWidth);
+          }}
+          className="bg-slate-300 hover:bg-slate-400 flex items-center  h-[calc(100vh-110px)]"
+        >
+          <Drag />
+        </section>
+        <section className="my-8 px-2 overflow-scroll overflow-x-hidden h-[calc(100vh-110px)]">
           {selectedNode ? (
             <Form id={selectedNode} />
           ) : (
